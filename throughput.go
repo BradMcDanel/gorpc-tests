b3a2d67809691a3061ade502c26880c33674423a
Brad <brad@hilburt.(none)>
2014-02-18 21:36:56 -0500

    spaces+tabs fixed

--- throughput.go
+++ throughput.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"net/rpc"
-	"net"
-	"time"
-	"net/http"
+    "fmt"
+    "os"
+    "net/rpc"
+    "net"
+    "time"
+    "net/http"
     "strconv"
 )
 
@@ -18,71 +18,67 @@ type Message int
 
 
 func (t *Message) Echo(args *DynArg, reply *DynArg) error {
-	reply.A = args.A 
-	return nil
+    reply.A = args.A 
+    return nil
 }
 
 func basicCall(c *rpc.Client, numBytes int) {
-	args := DynArg{A: make([]byte, numBytes) };
-	var reply DynArg
-	err := c.Call("Message.Echo", args, &reply)
-	checkError(err)
+    args := DynArg{A: make([]byte, numBytes) };
+    var reply DynArg
+    err := c.Call("Message.Echo", args, &reply)
+    checkError(err)
 }
 
 func checkError(err error) {
-	if err != nil {
-		fmt.Println("Fatal error ", err.Error())
-		os.Exit(1)
-	}
+    if err != nil {
+        fmt.Println("Fatal error ", err.Error())
+        os.Exit(1)
+    }
 }
 
 func startTCPServer(port string) (*rpc.Server) {
+    tcpAddr, err := net.ResolveTCPAddr("tcp", ":" +  port)
+    checkError(err)
 
-	tcpAddr, err := net.ResolveTCPAddr("tcp", ":" +  port)
-	checkError(err)
+    listener, err := net.ListenTCP("tcp", tcpAddr)
+    checkError(err)
 
-	listener, err := net.ListenTCP("tcp", tcpAddr)
-	checkError(err)
+    newServer := rpc.NewServer()
 
+    message := new(Message)
+    newServer.Register(message)
 
-	newServer := rpc.NewServer()
+    go newServer.Accept(listener)
 
-	message := new(Message)
-	newServer.Register(message)
-
-	go newServer.Accept(listener)
-	
     return newServer
 }
 
 func startHTTPServer(port string) (*rpc.Server) {
+    newServer := rpc.NewServer()
+    newServer.HandleHTTP(rpc.DefaultRPCPath, rpc.DefaultDebugPath)
+    
+    message := new(Message)
+    newServer.Register(message)
+    
+    l, e := net.Listen("tcp", port)
+    checkError(e)
 
-	newServer := rpc.NewServer()
-	newServer.HandleHTTP(rpc.DefaultRPCPath, rpc.DefaultDebugPath)
-
-	message := new(Message)
-	newServer.Register(message)
-	
-	l, e := net.Listen("tcp", port)
-	checkError(e)
-
-	go http.Serve(l, nil)
+    go http.Serve(l, nil)
 	
     return newServer
 }
 
 func startTCPClient(serverAddress string, port string) (*rpc.Client) {
-	client, err := rpc.Dial("tcp", serverAddress + ":" + port)
-	checkError(err)
-
-	return client
+    client, err := rpc.Dial("tcp", serverAddress + ":" + port)
+    checkError(err)
+    
+    return client
 }
 
 func startHTTPClient(serverAddress string, port string) (*rpc.Client) {
-	client, err := rpc.DialHTTP("tcp", serverAddress + ":" + port)
-	checkError(err)
-
-	return client
+    client, err := rpc.DialHTTP("tcp", serverAddress + ":" + port)
+    checkError(err)
+    return client
 }
 
 func throughputTest(serverAddr string, numClients int, numServers int, numWindows int, messageSize int) {
@@ -114,7 +110,6 @@ func throughputTest(serverAddr string, numClients int, numServers int, numWindow
     for i := 0; i < numClients; i++ {
         for j := 0; j < numWindows; j++ {
             basicCall(clients[i], messageSize)            
-
         }
     }       
 
@@ -128,12 +123,12 @@ func throughputTest(serverAddr string, numClients int, numServers int, numWindow
 }
 
 func main() {
-	if len(os.Args) != 2 {
-		fmt.Println("Usage: ", os.Args[0], "[server]")
-		os.Exit(1)
+    if len(os.Args) != 2 {
+        fmt.Println("Usage: ", os.Args[0], "[server]")
+        os.Exit(1)
 	}
 
-	serverAddr := os.Args[1]
+    serverAddr := os.Args[1]
 
     //args - serverAddr, numClients, numServers, numWindows, msgSize in bytes
     throughputTest(serverAddr, 200, 30, 5, 100)
